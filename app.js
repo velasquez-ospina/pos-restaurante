02cc58f1325d4e0850830a8742864800fc06260e
david velasquez ospina <davidvo22@hotmail.com>
2026-02-28 14:55:50 -0500

    historial

--- app.js
+++ app.js
@@ -19,7 +19,7 @@
 // 1. CONFIGURACIÓN
 // ─────────────────────────────────────────────
 const CONFIG = {
-    SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbz9a27PaRJZaYPyKL_2NrOTQTYw65xB7xGfiKSLcC9lVzwxjLUv2S08-3KsX-Cfk_Q/exec',
+    SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbys11HM_7fdV2wRzAGjVmRK-3IuuMyRDx-K24BtHUQDbl0KJMzx-IICOo588g9sz7Kk/exec',
     TOAST_DURATION_MS:        2000,
     SYNC_INTERVAL_MS:         10_000,
     SECURITY_CLICK_THRESHOLD: 5,
